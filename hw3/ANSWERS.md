b83a0cf31a9930193956c21bfc2e184fd5895db4
Tiger Fu <tigerfu@berkeley.edu>
2018-03-01 00:23:05 -0800

    start hw3

---
+++ hw3/ANSWERS.md
@@ -0,0 +1,15 @@
+## Questions
+
+1. What does the second 'nil' argument in the line 6 text_field_tag of teachers/new.html.erb represent?
+
+
+2. Go to `localhost:3000/teachers` in your browser; why does this not work?
+
+
+3. What type of request did your browser just perform?
+
+
+4. Go back to `localhost:3000/teachers/new`; submit the form again. What URL do you end up at?
+
+
+5. Why does `localhost:3000/teachers` work now?
---
+++ hw3/app/controllers/teachers_controller.rb
@@ -0,0 +1,19 @@
+class TeachersController < ApplicationController
+  def new
+    @placeholder_course = '186'
+  end
+
+  def create
+    # Hint: params??
+
+    # YOUR CODE HERE
+
+
+    # This line is explicitly declaring that this controller method should render/serve the
+    # 'views/teachers/show.html.erb' view. Otherwise, the controller would try to look for a 
+    # 'views/teachers/create.html.erb' view file which we do not have. 
+    # By convention, create is associated with a POST/PUT method while show is associated with the GET HTTP method
+    # so the result after a "create" (C in CRUD) is a show.
+    render 'show'
+  end
+end
